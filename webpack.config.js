7a5282db3bd9bd26906d47882483fc47418f259f
Vladislav <samokhvalov.0202@mail.ru>
2023-09-09 21:36:33 +0300

    setting up the environment

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[hash].js'
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.scss'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@helpers': path.resolve(__dirname, 'src/helpers')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ESLintPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Currency-tracker',
+      template: './public/index.html',
+      favicon: './public/favicon.png'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
